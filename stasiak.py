c7b1f18b2da6bc92005b00181e4438e313845e36
Artur Rzemek <artur.rzemek@student.wat.edu.pl>
2022-10-29 15:28:36 +0200

    xd

--- stasiak.py
+++ stasiak.py
@@ -9,12 +9,12 @@ import part_3
 import part_4
 
 header = {
-    'authorization': 'ODAzODgyMTM2NzI0OTYzMzYx.GuHHPW.7HHb5FS575KzEA0m1KJ-9xsW6mjTrHb3kWD_uw'
+    'authorization': 'ODAzODgyMTM2NzI0OTYzMzYx.GZswE0.QTgfn5dQQXZ4M_eDhSI4FtG91DuyeVx8rQ4UeQ'
 }
 x = "https://discord.com/api/v9/channels/1030430373206839387/messages"
 
 while True:
     message = {'content': random.choice(part_1.arr) + " " + random.choice(part_2.arr) + ", " + random.choice(part_3.arr) +" " + random.choice(part_4.arr)}
     r = requests.post(x, data=message, headers=header) 
-    time.sleep(60 * 60 * 12)
+    time.sleep(60 * 60 * random.randrange(6,24))
     
